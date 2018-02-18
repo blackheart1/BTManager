88e6120701deded632522e24978e5c75f864a193
black_heart <36564911+blackheart1@users.noreply.github.com>
2018-02-18 08:49:37 -0500

    repair

--- setup/sql/cache_con.sql
+++ setup/sql/cache_con.sql
@@ -1,4 +1,4 @@
 INSERT INTO `#prefix#_cache_con` (`name`, `value`) VALUES
 ('sql_time', '200'),
 ('theme_time', '30'),
-('cache_dir', '/home/btmaehfh/public_html/demo/cache');
+('cache_dir', 'cache');
