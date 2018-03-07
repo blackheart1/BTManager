ca1b4591fe9025a00418d2f6184858b017c7f7af
Thor10 <btmngmt@gmail.com>
2018-03-07 00:03:13 +0000

    Language Update
    Language Spell Checked And New Header Added

--- language/attachment/english.php
+++ language/attachment/english.php
@@ -1,72 +1,69 @@
 <?php
-/*
-*-----------------------------phpMyBitTorrent V 2.0.5--------------------------*
-*--- The Ultimate BitTorrent Tracker and BMS (Bittorrent Management System) ---*
-*--------------   Created By Antonio Anzivino (aka DJ Echelon)   --------------*
-*-------------               http://www.p2pmania.it               -------------*
-*------------ Based on the Bit Torrent Protocol made by Bram Cohen ------------*
-*-------------              http://www.bittorrent.com             -------------*
-*------------------------------------------------------------------------------*
-*------------------------------------------------------------------------------*
-*--   This program is free software; you can redistribute it and/or modify   --*
-*--   it under the terms of the GNU General Public License as published by   --*
-*--   the Free Software Foundation; either version 2 of the License, or      --*
-*--   (at your option) any later version.                                    --*
-*--                                                                          --*
-*--   This program is distributed in the hope that it will be useful,        --*
-*--   but WITHOUT ANY WARRANTY; without even the implied warranty of         --*
-*--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          --*
-*--   GNU General Public License for more details.                           --*
-*--                                                                          --*
-*--   You should have received a copy of the GNU General Public License      --*
-*--   along with this program; if not, write to the Free Software            --*
-*-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA --*
-*--                                                                          --*
-*------------------------------------------------------------------------------*
-*------              ©2005 phpMyBitTorrent Development Team              ------*
-*-----------               http://phpmybittorrent.com               -----------*
-*------------------------------------------------------------------------------*
-*-----------------  Thursday, November 04, 2010 9:05 PM   ---------------------*
-*/
+
 /**
-*
-* @package phpMyBitTorrent
-* @version $Id: english.php 1 2010-11-04 00:22:48Z joeroberts $
-* @copyright (c) 2010 phpMyBitTorrent Group
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License
-*
-*/
+**********************
+** BTManager v3.0.1 **
+**********************
+** http://www.btmanager.org/
+** https://github.com/blackheart1/BTManager
+** http://demo.btmanager.org/index.php
+** Licence Info: GPL
+** Copyright (C) 2018
+** Formerly Known As phpMyBitTorrent
+** Created By Antonio Anzivino (aka DJ Echelon)
+** And Joe Robertson (aka joeroberts)
+** Project Leaders: Black_heart, Thor.
+** File faq_manager.php 2018-03-02 07:39:00 Thor
+**
+** CHANGES
+**
+** 2018-03-02 - Added New Masthead
+** 2018-03-02 - Added New !defined('IN_PMBT')
+** 2018-03-02 - Fixed Spelling
+**/
+
+if (!defined('IN_PMBT'))
+{
+    include_once './../../security.php';
+    die ("You Can't Access This File Directly");
+}
 define("attach_lang_set",true);
-define("GENERAL_UPLOAD_ERROR","Could not upload attachment to %s.");
-define("IMAGE_FILETYPE_INVALID",'Image file type %d for mimetype %s not supported.');
-define("IMAGE_FILETYPE_MISMATCH",'Image file type mismatch: expected extension %1$s but extension %2$s given.');
-define("ATTACHED_IMAGE_NOT_IMAGE",'The image file you tried to attach is invalid.');
-define("UNABLE_GET_IMAGE_SIZE",'It was not possible to determine the dimensions of the image.');
+define("GENERAL_UPLOAD_ERROR","Could NOT Upload Attachment to %s.");
+define("IMAGE_FILETYPE_INVALID",'Image File Type %d for Mimetype %s NOT Supported.');
+define("IMAGE_FILETYPE_MISMATCH",'Image File Type Mismatch: Expected Extension %1$s but Extension %2$s given.');
+define("ATTACHED_IMAGE_NOT_IMAGE",'The Image File you tried to Attach is Invalid.');
+define("UNABLE_GET_IMAGE_SIZE",'It was NOT possible to determine the Dimensions of the Image.');
 define("MB","MB");
 define("KB","KB");
 define("BYTES","Bytes");
-define("WRONG_FILESIZE",'The file is too big, maximum allowed size is %1d %2s.');
-define("WRONG_SIZE",'The image must be at least %1$d pixels wide, %2$d pixels high and at most %3$d pixels wide and %4$d pixels high. The submitted image is %5$d pixels wide and %6$d pixels high.');
-define("EMPTY_FILEUPLOAD",'The uploaded file is empty.');
-define("PHP_SIZE_NA",'The attachment’s file size is too large.<br />Could not determine the maximum size defined by PHP in php.ini.');
-define("PHP_SIZE_OVERRUN",'The attachment’s file size is too large, the maximum upload size is %d MB.<br />Please note this is set in php.ini and cannot be overridden.');
-define("NOT_UPLOADED",'File could not be uploaded.');
-define("URL_INVALID",'The URL you specified is invalid.');
-define("URL_NOT_FOUND","The file specified could not be found.");
-define("EMPTY_REMOTE_DATA","File could not be uploaded, please try uploading the file manually.");
-define("PARTIAL_UPLOAD","The uploaded file was only partially uploaded.");
-define("DISALLOWED_EXTENSION",'The extension %s is not allowed.');
-define("_bt_fm_del_attach","Delete attachment");
-define("_bt_fm_del_attach_CONFIRM","Are you sure you want to delete this attachment?");
+define("WRONG_FILESIZE",'The File is too Big, Maximum Allowed Size is %1d %2s.');
+define("WRONG_SIZE",'The Image must be at least %1$d Pixels Wide, %2$d Pixels High and at most %3$d Pixels Wide and %4$d Pixels High. The Submitted Image is %5$d Pixels Wide and %6$d Pixels High.');
+
+define("EMPTY_FILEUPLOAD",'The Uploaded File is Empty.');
+define("PHP_SIZE_NA",'The Attachment’s File Size is too Large.<br />Could NOT determine the Maximum Size Defined by PHP in php.ini.');
+
+define("PHP_SIZE_OVERRUN",'The Attachment’s File Size is too Large, the Maximum Upload Size is %d MB.<br />Please Note this is Set in php.ini and can NOT be Overridden.');
+
+define("NOT_UPLOADED",'File Could NOT be Uploaded.');
+define("URL_INVALID",'The URL you Specified is Invalid.');
+define("URL_NOT_FOUND","The File Specified could NOT be Found.");
+define("EMPTY_REMOTE_DATA","File could NOT be Uploaded.  Please Try Uploading the file Manually!");
+define("PARTIAL_UPLOAD","The Uploaded File was Only Partially Uploaded.");
+define("DISALLOWED_EXTENSION",'The Extension %s is NOT Allowed.');
+define("_bt_fm_del_attach","Delete Attachment");
+define("_bt_fm_del_attach_CONFIRM","Are you sure you want to Delete this Attachment?");
 define("RETURN_UCP",'%sReturn to the User Control Panel%s');
-define("ATTACHMENTS_DELETED",'Attachments successfully deleted.');
-define("ATTACHMENT_DELETED",'Attachment successfully deleted.');
+define("ATTACHMENTS_DELETED",'Attachments Successfully Deleted.');
+define("ATTACHMENT_DELETED",'Attachment Successfully Deleted.');
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
 $lang = array_merge($lang, array(
-	'_FILENAME'	=> 'File Name',
-	'_FILE_COMMENT'	=> 'File Comment',
+    '_FILENAME'     => 'File Name',
+    '_FILE_COMMENT' => 'File Comment',
 ));
+
 ?>
