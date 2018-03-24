64d8c59648bfcb003bf72145ab0ce581da4f6fc2
Thor10 <btmngmt@gmail.com>
2018-03-24 10:06:11 +0000

    Language Check
    Added in Language Check

--- language/admin/acp_prune/english.php
+++ language/admin/acp_prune/english.php
@@ -13,50 +13,23 @@
 ** Created By Antonio Anzivino (aka DJ Echelon)
 ** And Joe Robertson (aka joeroberts)
 ** Project Leaders: Black_heart, Thor.
-** File acp_prune/english.php 2018-02-28 07:53:00 Thor
+** File acp_prune/english.php 2018-03-24 09:44:00 Thor
 **
 ** CHANGES
 **
-** 2018-02-23 - Added New Masthead
-** 2018-02-23 - Added New !defined('IN_PMBT')
-** 2018-02-23 - Fixed Spelling
+** 2018-03-24 - Added New !defined('IN_PMBT')
+** 2018-03-24 - Fixed Language Check
 **/
 
 if (!defined('IN_PMBT'))
 {
-    ?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html>
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    include_once './../../security.php';
+    die ("You can't access this file directly");
+}
 
-            <title>
-        <?php if (isset($_GET['error']))
-            {
-        echo htmlspecialchars($_GET['error']);
-            }
-            ?> Error</title>
-
-            <link rel='stylesheet' type='text/css' href='/errors/error-style.css' />
-        </head>
-
-        <body>
-            <div id='container'>
-        <div align='center' style='padding-top: 15px'>
-            <img src='/errors/error-images/alert.png' width='89' height='94' alt='' title='' />
-        </div>
-
-        <h1 class='title'>Error 404 - Page Not Found</h1>
-        <p class='sub-title' align='center'>The page that you are looking for does not appear to exist on this site.</p>
-        <p>If you typed the address of the page into the address bar of your browser, please check that you typed it in correctly.</p>
-        <p>If you arrived at this page after you used an old Bookmark or Favourite, the page in question has probably been moved. Try locating the page via the navigation menu and then update your bookmarks.</p>
-            </div>
-        </body>
-    </html>
-
-    <?php
-    exit();
+if (empty($lang) || !is_array($lang))
+{
+    $lang = array();
 }
 
 $lang = array_merge($lang, array(
