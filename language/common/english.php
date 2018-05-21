63a70804c5dde2ed674b012a2e321a4614be4c85
Thor10 <btmngmt@gmail.com>
2018-05-21 06:05:15 +0100

    Language & Template Update
    Language & Template Update

--- language/common/english.php
+++ language/common/english.php
@@ -572,7 +572,7 @@ $lang = array_merge($lang, array(
     '_BBCODE_U_HELP'        => 'Underline Text: [u]text[/u]',
     '_BBCODE_W_HELP'        => 'Insert URL: [url]http://url[/url] OR [url=http://url]URL text[/url]',
     '_BBCODE_D_HELP'        => 'Flash: [flash=width,height]http://url[/flash]',
-    '_STYLES_TIP'           => 'Tip:  Styles can be Applied Quickly to Selected Text.',
+    '_STYLES_TIP'           => 'Tip: Styles can be Applied Quickly to Selected Text.',
     '_SMILIES'              => 'Smilies',
     '_MORE_SMILIES'         => 'More Smilies',
     'MORE_SMILIES'          => 'More Smilies',
