7a7121512bbc9dd24f93bbd52d59ff4330aaecec
black_heart <36564911+blackheart1@users.noreply.github.com>
2018-05-26 05:31:44 -0400

    repair DOCTYPE

--- file.php
+++ file.php
@@ -40,13 +40,14 @@ if (!function_exists('htmlspecialchars_decode'))
 }
 function wrap_img_in_html($src, $title)
 {
+	global $user, $version;
 	echo '
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-        <html xmlns="http://www.w3.org/1999/xhtml" dir="{S_CONTENT_DIRECTION}" lang="{S_USER_LANG}" xml:lang="{S_USER_LANG}">
+        <html xmlns="http://www.w3.org/1999/xhtml" dir="' . $user->lang['DIRECTION'] .'" lang="' .$user->lang['USER_LANG'] . '" xml:lang="' .$user->lang['USER_LANG'] . '">
             <head>
                 <meta name="generator" content="HTML Tidy for Linux (vers 6 November 2007), see www.w3.org">
-                <meta http-equiv="Content-Type" content="text/html; charset={S_CONTENT_ENCODING}">
-                <meta name="generator" content="PMBT {PMBT_VER}">
+                <meta http-equiv="Content-Type" content="text/html; charset=' .$user->lang['CONTENT_ENCODING'] . '">
+                <meta name="generator" content="BTManager ' . $version . '">
                 <meta http-equiv="PRAGMA" content="NO-CACHE">
                 <meta http-equiv="EXPIRES" content="-1">
                 <meta http-equiv="Cache-Control" content="no-cache">
