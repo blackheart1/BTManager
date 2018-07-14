9c0bd966f06f3116f059b1145b6089f2b935d38b
Thor10 <btmngmt@gmail.com>
2018-07-14 09:03:21 +0100

    Language & Theme Update
    Language & Theme Update

--- language/polls/english.php
+++ language/polls/english.php
@@ -13,13 +13,14 @@
 ** Created By Antonio Anzivino (aka DJ Echelon)
 ** And Joe Robertson (aka joeroberts)
 ** Project Leaders: Black_heart, Thor.
-** File polls/english.php 2018-03-20 11:08:00 Thor
+** File polls/english.php 2018-07-14 08:41:00 Thor
 **
 ** CHANGES
 **
 ** 2018-03-02 - Added New Masthead
 ** 2018-03-02 - Added New !defined('IN_PMBT')
 ** 2018-03-02 - Fixed Spelling
+** 2018-07-14 - Added/Amended Language
 **/
 
 if (!defined('IN_PMBT'))
@@ -34,12 +35,13 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-    'POLL_OVER_VIEW'      => 'Polls Overview',
-    'POLL_QUESTION'       => 'Poll Questions',
+    'POLL_OVER_VIEW'      => 'Poll Overview',
+    'POLL_ANSWERS'        => 'Poll Answers',
     'POLL_OPTION_NUM'     => 'Poll Option Number',
-    'POLL_USER_OVER_VIEW' => 'Polls User Overview',
-    'SELECTION'           => 'Selection',
+    'POLL_USER_OVER_VIEW' => 'Poll User Overview',
+    'SELECTION'           => 'Selected',
     'POLL_REMOVED'        => 'Poll was Successfully Removed',
+    'ANSWERS'             => 'Answers',
 ));
 
 ?>
